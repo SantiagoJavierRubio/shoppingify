c4ad6d96b0860b0e7ec6b6843a2ffd873c35d304
Santiago Javier Rubio <s.rubio91@gmail.com>
2022-01-27 00:57:46 -0300

    safari cross-site error no-fix

--- server/index.js
+++ server/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
 const session = require('express-session');
-const sessionStore = require('express-session-rsdb')
+const MySQLStore = require('express-mysql-session')(session);
+const db = require('./db.js');
 
 
 const userRoutes = require('./routes/users.js');
@@ -13,24 +15,41 @@ const statsRoutes = require('./routes/stats.js');
 const app = express();
 dotenv.config();
 
-app.set('trust proxy', 1);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
+app.use(cookieParser());
+const sessionStore = new MySQLStore({}, db.promise())
 app.use(session({
-    store: new sessionStore(),
+    store: sessionStore,
     secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
-    cookie: { maxAge: 1000 * 60 * 24, secure: true, sameSite: 'none' }
+    cookie: { 
+        maxAge: 1000 * 60 * 24,
+        secure: true,
+        sameSite: 'none',
+        httpOnly: false,
+    }
 }))
-app.use(express.json());
+app.set('trust proxy', 1);
 app.use(cors({ credentials: true, origin: 'https://shoppingify-sjr.netlify.app' }));
 
 const PORT = process.env.PORT || 5000;
-
 
 app.use('/users', userRoutes);
 app.use('/products', productRoutes);
 app.use('/lists', listRoutes);
 app.use('/stats', statsRoutes);
+
+app.use(function(req, res, next){
+    res.set('credentials', 'include');
+    res.set('Access-Control-Allow-Credentials', true);
+    res.set('Access-Control-Allow-Origin', req.headers.origin);
+    res.set('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.set('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
+    next();
+})
+
 
 // error handler
 app.use(function(err, req, res, next) {
